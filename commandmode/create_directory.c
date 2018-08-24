c25ea8c518bd3a5081f191ea0000eeb03d072c98
neil <nilabja10201992@gmail.com>
2018-08-25 02:38:01 +0530

    create directort function

---
+++ commandmode/create_directory.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "create_directory.h"
+
+int create_directory(char *directory_name, char *path_for_directory){
+    struct stat status = {0};
+    strcat(path_for_directory, "/");
+    strcat(path_for_directory,directory_name);
+    if (stat(path_for_directory, &status) == -1) {
+        mkdir(path_for_directory, 0700);
+        return 1;
+    }   
+    return 0;
+}
