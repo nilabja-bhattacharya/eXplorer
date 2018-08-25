4db634f969ee189e2b39138cfb8702170c699362
neil <nilabja10201992@gmail.com>
2018-08-25 16:27:20 +0530

    Code for copy directory

---
+++ commandmode/copy_directory.h
@@ -0,0 +1,4 @@
+#ifndef TERMINALEXPLORER_COMMANDMODE_COPY_DIRECTORY_H
+#define TERMINALEXPLORER_COMMANDMODE_COPY_DIRECTORY_H
+int copy_directory(char *path_name_for_directory_to_be_copied, char *path_to_new_directory);
+#endif //TERMINALEXPLORER_COMMANDMODE_COPY_DIRECTORY_H
